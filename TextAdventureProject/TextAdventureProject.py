52a3456a3c8e027ad00b5ce383984d2f25e26db2
MattHackenbrook <matthew.hackenbrook@ryerson.ca>
2021-01-15 22:06:21 -0500

    Initial commit

---
+++ TextAdventureProject/TextAdventureProject.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import Models
+import Saves
+import src
+#from Models import PlayerModel
+#from Models.PlayerModel import player
+
+p = Models.PlayerModel.player("name")
+
+print(p.PlayerName)
+
+#print(sys.path)
