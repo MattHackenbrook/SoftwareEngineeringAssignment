13fef69c0fd9c7ef3b21fee52083d66e2e81f318
MattHackenbrook <77515289+MattHackenbrook@users.noreply.github.com>
2021-01-30 14:59:43 -0500

    Add files via upload

---
+++ exampleScript.ps1
@@ -0,0 +1,15 @@
+﻿$outputFile = "C:\Users\Matthew\Desktop\textfile.txt" #change me
+
+$knifeTraits = @{"damage" = 10; "durability" = 3}
+$knife = @{"name" = "knife"; "traits" = $knifeTraits}
+$cocaineTraits = @{"strength" = 5; "uses" = 1}
+$cocaine = @{"name" = "cocaine"; "traits" = $cocaineTraits}
+$dresserContents = @($knife, $cocaine)
+$dresser = @{"name" = "dresser"; "content" = $dresserContents}
+$sock = @{"name" = "sock"}
+$boxContents = @($sock)
+$box = @{"name" = "cardboard box"; "content" = $boxContents}
+$containers = @($dresser, $box)
+$cell = @{"name" = "my cell"; "containers" = $containers}
+
+$cell | ConvertTo-Json -Depth 10 | Out-File -FilePath $outputFile
