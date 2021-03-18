374c192ed2ec326aa0f11089bd09a004316f187d
MattHackenbrook <77515289+MattHackenbrook@users.noreply.github.com>
2021-03-18 16:41:13 -0400

    Add files via upload

--- TextAdventureProject/DataModels.py
+++ TextAdventureProject/DataModels.py
@@ -1,8 +1,17 @@
+import json
+
+
 class Room:
     def __init__(self, doors, containers, characters, shortDesc, longDesc, visited):
         self.doors = doors
+        for key in self.doors:
+            self.doors[key] = Door(**(self.doors[key]))
         self.containers = containers
+        for key in self.containers:
+            self.containers[key] = Container(**(self.containers[key]))
         self.characters = characters
+        for character in self.characters:
+            self.characters[character] = Character(**(self.characters[character]))
         self.shortDesc = shortDesc
         self.longDesc = longDesc
         self.visited = visited
@@ -15,21 +24,31 @@ class Door:
 
 
 class Container:
-    def __init__(self, items, desc):
+    def __init__(self, items=None, desc=None):
         self.items = items
+        try:
+            for item in self.items:
+                self.items[item] = Item(**(self.items[item]))
+        except TypeError:
+            pass
         self.desc = desc
 
 
 class Character:
-    def __init__(self, characterClass, stats, inv, wearing):
-        self.characterClass = characterClass
+    def __init__(self, classification, stats, inv, wearing, desc):
+        self.classification = classification
         self.stats = stats
         self.inv = inv
+        for item in self.inv:
+            inv[item] = Item(**(inv[item]))
         self.wearing = wearing
+        for item in self.wearing:
+            wearing[item] = Item(**(wearing[item]))
+        self.desc = desc
 
 
 class Item:
-    def __init__(self, trait, itemClass, desc):
-        self.trait = trait
-        self.itemClass = itemClass
+    def __init__(self, traits, classification, desc):
+        self.traits = traits
+        self.classification = classification
         self.desc = desc
