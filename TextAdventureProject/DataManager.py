14ff47fa837c7bfc8d95f721e678640705e7c12b
MattHackenbrook <77515289+MattHackenbrook@users.noreply.github.com>
2021-03-18 16:39:54 -0400

    Add files via upload

---
+++ TextAdventureProject/DataManager.py
@@ -0,0 +1,34 @@
+import json
+from json import JSONEncoder
+import DataModels
+
+
+class RoomEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
+class DataManager:
+
+    def __init__(self):
+        self.rooms = {}
+        self.filePath = 'StartFile.json'
+        self.buildRoomsFromData()
+        self.filePath = 'SaveFile.json'
+
+    def readData(self):
+        with open(self.filePath, 'r') as saveFile:
+            return json.load(saveFile)
+
+    def writeData(self):
+        with open(self.filePath, 'w') as saveFile:
+            roomsJson = json.dump(self.rooms, saveFile, indent=4, cls=RoomEncoder)
+
+    def buildRoomsFromData(self):
+        data = self.readData()
+        for room in data:
+            self.rooms[room] = DataModels.Room(**data[room])
+
+
+x = DataManager()
+x.writeData()
