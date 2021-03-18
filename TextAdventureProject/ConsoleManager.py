56c81ffabafae1cbf999c72126bd9e575450e688
Nabil Haque <nabil.haque24@hotmail.com>
2021-03-18 18:50:17 -0400

    Added helper and ReadUserInput functions

--- TextAdventureProject/ConsoleManager.py
+++ TextAdventureProject/ConsoleManager.py
@@ -8,25 +8,41 @@ Created on Sat Mar 13 13:52:34 2021
 import CompileOutput
 import Translator
 import WorldHandler
+
+
 class ConsoleManager:
     
-    def __init__(self, room, command):
+    def __init__(self, room):
         self.room = room
-        self.command = command
+        self.userInput = ""
             
-    
+    def readUserInput(self):
+        self.userInput = input("... ")
+        return self.userInput
+        
+        
     def printRoom(self):
         return CompileOutput().playerRoomDescription(self.room)
         
     
-    def printSuccess(self):
-        if Translator().checkValidInput(self.command) == True:
-            print("Sucess.")
-        else:
-            print("Invalid input. Please try again")
+    def printFailure(self):
+        if Translator().checkValidInput(self.userInput) == False:
+             print("Invalid input. Please try again")
+             self.userInput = input("\n... ")
     
     def endGame(self)
         if WorldHandler().checkEnd() == True:
         print("Game Over")
     
-            
+    def helper(self):
+        print('''You have 7 different commands you can use in the game.
+                 1.Use
+                 2.Throw
+                 3.Hit
+                 4.Take
+                 5.Inspect
+                 6.Speak
+                 7.Go 
+                  ''' + self.room["LongDesc"] )
+                  
+                
