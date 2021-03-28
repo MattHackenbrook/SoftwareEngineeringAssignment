26837df796145b12fb539427488bdf28d44760b5
jath-k <77517780+jath-k@users.noreply.github.com>
2021-03-28 19:39:52 -0400

    Update parserCommand.py

--- TextAdventureProject/parserCommand.py
+++ TextAdventureProject/parserCommand.py
@@ -13,12 +13,26 @@ def parserCommand(Action):
         choice = str(choice)
     return choice
 
-
-def parser(input):
+def parse_input(input):
     command = ""
     command_words = list(input.split(" "))
     new_command = []
+    
     for word in command_words:
-        if word not in Quit_words:
-            if word in Action:
-                new_command.append(Action[word])
+        if word not in QUIT_HELP:
+            if word in doorList:
+                new_command.append(doorList[word])
+        if word not in QUIT_HELP:
+            if word in containerList:
+                new_command.append(containerList[word])
+        if word not in QUIT_HELP:
+            if word in itemList:
+                new_command.append(itemList[word])
+        if word not in QUIT_HELP:
+            if word in characterList:
+                new_command.append(characterList[word])
+
+    for word in new_command[:2]:
+        command = command + word + " "
+    command = command.strip()
+    return command
