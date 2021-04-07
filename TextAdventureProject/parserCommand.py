2747c2e21751241f8c0ae268788c3750fb2eb0d8
Apivato <58273398+Apivato@users.noreply.github.com>
2021-04-06 22:02:33 -0400

    update to parse input

--- TextAdventureProject/parserCommand.py
+++ TextAdventureProject/parserCommand.py
@@ -47,12 +47,12 @@ def parseInput(console, owner):
                 command["Target"] = word.title()
             elif command["Action"] == Action.UNLOCK:
                 if doorObjectsList[word.title()].locked == False:  # check to see if door is even able to be unlocked, if door is already unlocked, u cannot unlock it again so returns invalid by setting target to empty string
-                    command["Target"] = ""
+                    command["Target"] = None
                 else:
-                    command["Target"] = word
+                    command["Target"] = word.title()
             elif command["Action"] == Action.ENTER:
                 if doorObjectsList[word.title()].locked == True:  # same here but for when u try to enter a locked door
-                    command["Target"] = ""
+                    command["Target"] = None
                 else:
                     command["Target"] = word.title()
             else:
