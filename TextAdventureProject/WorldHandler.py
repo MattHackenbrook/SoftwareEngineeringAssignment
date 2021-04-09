e0ed9043056f1628e6b8c6f093821ab244146e30
Apivato <58273398+Apivato@users.noreply.github.com>
2021-04-08 21:14:24 -0400

    again

--- TextAdventureProject/WorldHandler.py
+++ TextAdventureProject/WorldHandler.py
@@ -146,7 +146,7 @@ class WorldHandler:
                         else:
                             extra += ", a " + item
             if cmd.target != "Ground":
-                return command.target.desc + extra
+                return "see " + command.target.desc + extra
             else:
                 return "inspect the floor." + extra
         except:
